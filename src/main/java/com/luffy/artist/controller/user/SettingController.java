08df0a2ed31a7ab9b784005e0417e1a83a4f8af3
Peng xue-tao <197106340@qq.com>
2020-06-16 22:43:58 +0800

    优化设置签名接口逻辑

--- src/main/java/com/luffy/artist/controller/user/SettingController.java
+++ src/main/java/com/luffy/artist/controller/user/SettingController.java
@@ -5,10 +5,7 @@ import com.luffy.artist.entity.UserSignature;
 import com.luffy.artist.enums.ErrorCode;
 import com.luffy.artist.service.SysDictService;
 import com.luffy.artist.service.UserSignatureService;
-import com.luffy.artist.vo.ConvertReq;
-import com.luffy.artist.vo.ConvertResp;
 import com.luffy.artist.vo.Result;
-import com.luffy.artist.vo.SignatureSwitchReq;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
@@ -19,8 +16,6 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Controller
 @Api("设置")
@@ -36,27 +31,29 @@ public class SettingController {
 
     /**
      * 设置签名开关
-     * @param signatureSwitchReq 设置签名开关请求参数
      * @return Result<SysDict>
      */
     @ResponseBody
     @PostMapping("/signatureSwitch")
     @ApiOperation(value = "设置签名开关")
-    public Result<SysDict> signatureSwitch(@RequestBody SignatureSwitchReq signatureSwitchReq) {
+    public Result<SysDict> signatureSwitch() {
         SysDict sysDict = sysDictService.querySysDictByTypeKey("signatureSwitch");
-        String signatureSwitch = signatureSwitchReq.getSignatureSwitchStatus();
         if (sysDict == null || StringUtils.isEmpty(sysDict.getSubtypeValue())) {
             return new Result<>(ErrorCode.FAILURE.getCode(), ErrorCode.FAILURE.getErrorDesc());
         }
         SysDict updateSysDict = new SysDict();
         updateSysDict.setId(sysDict.getId());
-        if ("0".equals(signatureSwitch)) {
-            updateSysDict.setSubtypeValue("1");
-            LOGGER.info("---已打开签名开关---");
-        }
-        if ("1".equals(signatureSwitch)) {
-            updateSysDict.setSubtypeValue("0");
-            LOGGER.info("---已关闭签名开关---");
+        switch (sysDict.getSubtypeValue()) {
+            case "0":
+                updateSysDict.setSubtypeValue("1");
+                LOGGER.info("---已打开签名开关---");
+                break;
+            case "1":
+                updateSysDict.setSubtypeValue("0");
+                LOGGER.info("---已关闭签名开关---");
+                break;
+            default:
+                return new Result<>(ErrorCode.FAILURE.getCode(), ErrorCode.FAILURE.getErrorDesc());
         }
         sysDictService.updateSysDict(updateSysDict);
         return new Result<>(ErrorCode.SUCCESS.getCode(), ErrorCode.SUCCESS.getErrorDesc(), sysDict);
